3cca88ccd82f48e1702bc0c6a45b5939f01cb188
Venkata ramana <vakamallaramana91@gmail.com>
2024-10-23 23:23:01 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ count = 4
 ami = "ami-0ddc798b3f1a5117e"
 instance_type = "t2.medium"
 key_name = "ramanapem"
-vpc_security_group_ids = ["sg-0e37b20d8f070958b"]
+vpc_security_group_ids = ["sg-05a41ab493052c23c"]
 tags = {
 Name = var.instance_names[count.index]
 }
